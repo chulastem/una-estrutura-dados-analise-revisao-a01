675d6ec0718f1d87a8357e77eec072eaab1108f2
Pedro Antonio <pedrozeek981@gmail.com>
2023-10-05 10:28:04 -0300

    Solução criada
    Agora o programa aceita respostas com letras minusculas e maiusculas.

--- Solução.cs
+++ Solução.cs
@@ -16,12 +16,12 @@
 
         int porcentagem = 0;
         int tentativas = 0;
-        string[] resposta = new string[4];
+        string?[] resposta = new string?[4];
 
         while (tentativas < 3)
         {
             Console.Write("Seu cartão de vacina está em dia? (SIM/NAO): ");
-            resposta[0] = Console.ReadLine() ?? "".Trim().ToUpper();
+            resposta[0] = (Console.ReadLine()??"").Trim().ToUpper();
 
             if (resposta[0] == "SIM")
             {
@@ -41,6 +41,7 @@
                 {
                     Console.WriteLine("Não foi possível realizar o diagnóstico.");
                     Console.WriteLine("Gentileza procurar ajuda médica caso apareça algum sintoma.");
+                    Console.ReadLine();
                     Environment.Exit(0);
                 }
             }
@@ -49,7 +50,7 @@
         while (tentativas < 3)
         {
             Console.Write("Teve algum dos sintomas recentemente? (dor de cabeça, febre, náusea, dor articular, gripe) (SIM/NAO): ");
-            resposta[1] = Console.ReadLine() ?? "".Trim().ToUpper();
+            resposta[1] = (Console.ReadLine()??"").Trim().ToUpper();
 
             if (resposta[1] == "SIM")
             {
@@ -69,6 +70,7 @@
                 {
                     Console.WriteLine("Não foi possível realizar o diagnóstico.");
                     Console.WriteLine("Gentileza procurar ajuda médica caso apareça algum sintoma.");
+                    Console.ReadLine();
                     Environment.Exit(0);
                 }
             }
@@ -77,7 +79,7 @@
         while (tentativas < 3)
         {
             Console.Write("Teve contato com pessoas com sintomas gripais nos últimos dias? (SIM/NAO): ");
-            resposta[2] = Console.ReadLine() ?? "".Trim().ToUpper() ?? "";
+            resposta[2] = (Console.ReadLine()??"").Trim().ToUpper();
 
             if (resposta[2] == "SIM")
             {
@@ -97,6 +99,7 @@
                 {
                     Console.WriteLine("Não foi possível realizar o diagnóstico.");
                     Console.WriteLine("Gentileza procurar ajuda médica caso apareça algum sintoma.");
+                    Console.ReadLine();
                     Environment.Exit(0);
                 }
             }
@@ -105,7 +108,7 @@
         while (tentativas < 3)
         {
             Console.Write("Está retornando de viagem realizada no exterior? (SIM/NAO): ");
-            resposta[3] = Console.ReadLine() ?? "".Trim().ToUpper();
+            resposta[3] = (Console.ReadLine()??"").Trim().ToUpper();
 
             if (resposta[3] == "SIM")
             {
@@ -126,6 +129,7 @@
                 {
                     Console.WriteLine("Não foi possível realizar o diagnóstico.");
                     Console.WriteLine("Gentileza procurar ajuda médica caso apareça algum sintoma.");
+                    Console.ReadLine();
                     Environment.Exit(0);
                 }
             }
